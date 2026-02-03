a9a643f0f0c6023dc90a84d4f1f29e5302686eab
Z1TK <204508328+Z1TK@users.noreply.github.com>
2026-02-03 20:02:16 +0300

    Models: post

--- backend/sqrt_chan/app/models/base.py
+++ backend/sqrt_chan/app/models/base.py
@@ -1,11 +1,16 @@
-from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import (DeclarativeBase, Mapped, declared_attr,
+                            mapped_column)
+
 
 class Base(DeclarativeBase, AsyncAttrs):
     __abstruct__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, )
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return cls.__name__.lower() + 's'
+        return cls.__name__.lower() + "s"
--- backend/sqrt_chan/app/models/thread.py
+++ backend/sqrt_chan/app/models/thread.py
@@ -1,13 +1,19 @@
-from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean, func, ForeignKey
 from datetime import datetime
 
+from sqlalchemy import Boolean, ForeignKey, String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
 class Thread(Base):
-    board_id: Mapped[int] = mapped_column(ForeignKey('boards.id'))
+    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id"))
     title: Mapped[str] = mapped_column(String(255))
-    is_closed: Mapped[bool] = mapped_column(Boolean, server_default='false')
-    is_archived: Mapped[bool] = mapped_column(Boolean, server_default='false')
+    is_closed: Mapped[bool] = mapped_column(Boolean, server_default="false")
+    is_archived: Mapped[bool] = mapped_column(Boolean, server_default="false")
     bump_count = Mapped[int] = mapped_column(default=0)
     bump_limit = Mapped[int] = mapped_column(default=100)
+    posts = Mapped[list["Post"]] = mapped_column(
+        "Post", back_populates="thread", cascade="all, delete-orphan"
+    )
     created_at = mapped_column[datetime] = mapped_column(server_default=func.now())
