bae0623ade74269a0a2e360be593645a39975f92
Z1TK <204508328+Z1TK@users.noreply.github.com>
2026-02-03 21:03:01 +0300

    Create config for db and connection db

---
+++ backend/sqrt_chan/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import SettingsConfigDict, BaseSettings
+import os
+
+class Settings(BaseSettings):
+    db_user: str
+    db_host: str
+    db_password: str
+    db_port: str
+    db_name: str
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../', '.env')),
+        env_ignore_empty=True,
+        env_prefix='APP_'
+    )
+
+    def get_db_url(self):
+        return f'postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}'
+
+setting = Settings()
